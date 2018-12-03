9b38f254a9755e3d1f1a85a3c25f7b31c2436488
feiwu <lv@haoyunma.com>
2018-12-03 18:12:24 +0800

    1.增加底部导航 2.主页结构搭建：多tab切滑动置顶，回顶部等功能

--- MyApplication/app/src/main/java/com/example/lv/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/lv/myapplication/MainActivity.java
@@ -1,71 +1,92 @@
 package com.example.lv.myapplication;
 
-import android.app.Activity;
-import android.content.Intent;
-import android.graphics.PixelFormat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Gravity;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.WindowManager;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
-import com.example.lv.myapplication.utils.SharedPreferencesHelper;
+import com.example.lv.myapplication.adapters.BaseFragmentPagerAdapter;
+import com.example.lv.myapplication.ui.BaseLazyFragment;
+import com.example.lv.myapplication.ui.maintab.TabDiscoveryFragment;
+import com.example.lv.myapplication.ui.maintab.TabMainFragment;
+import com.example.lv.myapplication.ui.maintab.TabMoreFragment;
+import com.example.lv.myapplication.ui.maintab.TabVideoFragment;
+import com.example.lv.myapplication.uiviews.navigations.BottomNavigationViewHelper;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import java.util.ArrayList;
+import java.util.List;
 
-    View nightView;
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViewById(R.id.close_dark_btn).setOnClickListener(this);
-        findViewById(R.id.open_dark_btn).setOnClickListener(this);
-        findViewById(R.id.open_first_act_btn).setOnClickListener(this);
-    }
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        //默认 >3 的选中效果会影响ViewPager的滑动切换时的效果，故利用反射去掉
+        BottomNavigationViewHelper.disableShiftMode(bottomNavigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.item_news:
+                                viewPager.setCurrentItem(0);
+                                break;
+                            case R.id.item_lib:
+                                viewPager.setCurrentItem(1);
+                                break;
+                            case R.id.item_find:
+                                viewPager.setCurrentItem(2);
+                                break;
+                            case R.id.item_more:
+                                viewPager.setCurrentItem(3);
+                                break;
+                        }
+                        return false;
+                    }
+                });
 
-    @Override
-    public void onClick(View view) {
-        final int id = view.getId();
-        if (id == R.id.open_dark_btn) {
-            if (!SharedPreferencesHelper.getBoolean("APP_NIGHT_MODE", false)) {
-                SharedPreferencesHelper.applyBoolean("APP_NIGHT_MODE", true);
-                nightView = changeActNightMode(this);
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
             }
-        } else if (id == R.id.close_dark_btn) {
-            if (SharedPreferencesHelper.getBoolean("APP_NIGHT_MODE", false)) {
-                SharedPreferencesHelper.applyBoolean("APP_NIGHT_MODE", false);
-                removeActNightMode(this, nightView);
+
+            @Override
+            public void onPageSelected(int position) {
+                if (menuItem != null) {
+                    menuItem.setChecked(false);
+                } else {
+                    bottomNavigationView.getMenu().getItem(0).setChecked(false);
+                }
+                menuItem = bottomNavigationView.getMenu().getItem(position);
+                menuItem.setChecked(true);
             }
-        } else if (id == R.id.open_first_act_btn) {
-            Intent intent = new Intent(this, FirstActivity.class);
-            startActivity(intent);
-        }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+            }
+        });
+        bottomNavigationView.setItemIconTintList(null);
+        setupViewPager(viewPager);
     }
 
-    private View changeActNightMode(Activity activity) {
-        WindowManager mWindowManager = activity.getWindowManager();//(WindowManager) getSystemService(Context.WINDOW_SERVICE);
-        WindowManager.LayoutParams nightViewParam = new WindowManager.LayoutParams(
-                WindowManager.LayoutParams.TYPE_APPLICATION,
-                WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE | WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
-                        | WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
-                        | WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION,
-                PixelFormat.TRANSPARENT);
-        nightViewParam.width = ViewGroup.LayoutParams.MATCH_PARENT;
-        nightViewParam.height = ViewGroup.LayoutParams.MATCH_PARENT;
-        nightViewParam.gravity = Gravity.CENTER;
-        View nightView = new View(this);
-        nightView.setBackgroundColor(0x66000000);
-        mWindowManager.addView(nightView, nightViewParam);
-        return nightView;
-
+    private void setupViewPager(ViewPager viewPager) {
+        String[] titles = {"主页", "视频", "发现", "更多"};
+        List<BaseLazyFragment> fragments = new ArrayList<>();
+        fragments.add(new TabMainFragment());
+        fragments.add(new TabVideoFragment());
+        fragments.add(new TabDiscoveryFragment());
+        fragments.add(new TabMoreFragment());
+        BaseFragmentPagerAdapter adapter = new BaseFragmentPagerAdapter(getSupportFragmentManager(), fragments, titles);
+        viewPager.setAdapter(adapter);
     }
 
-    private void removeActNightMode(Activity activity, View view) {
-        if (null == view)
-            return;
-        WindowManager mWindowManager = activity.getWindowManager();// (WindowManager) getSystemService(Context.WINDOW_SERVICE);
-        mWindowManager.removeViewImmediate(view);
-    }
+    private ViewPager viewPager;
+    private MenuItem menuItem;
+    private BottomNavigationView bottomNavigationView;
 }
---
+++ MyApplication/app/src/main/java/com/example/lv/myapplication/uiviews/navigations/BottomNavigationViewHelper.java
@@ -0,0 +1,40 @@
+package com.example.lv.myapplication.uiviews.navigations;
+
+import android.annotation.SuppressLint;
+import android.support.design.internal.BottomNavigationItemView;
+import android.support.design.internal.BottomNavigationMenuView;
+import android.support.design.widget.BottomNavigationView;
+
+import java.lang.reflect.Field;
+
+/**
+ * Created by lv on 18-11-30.
+ * 当菜单项多于3个时，效果和3个及以下的效果已经完全不一样了
+ * 利用反射改变mShiftingMode值为true
+ */
+
+public class BottomNavigationViewHelper {
+
+    @SuppressLint("RestrictedApi")
+    public static void disableShiftMode(BottomNavigationView navigationView) {
+
+        BottomNavigationMenuView menuView = (BottomNavigationMenuView) navigationView.getChildAt(0);
+        try {
+            Field shiftingMode = menuView.getClass().getDeclaredField("mShiftingMode");
+            shiftingMode.setAccessible(true);
+            shiftingMode.setBoolean(menuView, false);
+            shiftingMode.setAccessible(false);
+
+            for (int i = 0; i < menuView.getChildCount(); i++) {
+                BottomNavigationItemView itemView = (BottomNavigationItemView) menuView.getChildAt(i);
+                itemView.setShiftingMode(false);
+                itemView.setChecked(itemView.getItemData().isChecked());
+            }
+
+        } catch (NoSuchFieldException e) {
+           //Unable to get shift mode fiel
+        } catch (IllegalAccessException e) {
+            //Unable to change value of shift mode
+        }
+    }
+}
